f9ab7477ef1548a6708b4177e64dfff34dc9a582
IgorAkh <pe3k4bo0@ya.ru>
2026-03-19 08:30:21 +0300

    Added sub- parent class: BaseBookingTest

--- src/test/java/tests/BaseBookingTest.java
+++ src/test/java/tests/BaseBookingTest.java
@@ -1,19 +1,42 @@
 package tests;
 
-import core.clients.APIClient;
-import io.restassured.mapper.ObjectMapper;
-import io.restassured.mapper.ObjectMapperDeserializationContext;
-import io.restassured.mapper.ObjectMapperSerializationContext;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import core.models.CreatedBooking;
+import core.models.NewBooking;
+import io.restassured.response.Response;
+import org.assertj.core.api.Assertions;
+import org.assertj.core.api.AssertionsForClassTypes;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
-import java.util.List;
+import static core.utils.SetupNewBookingFields.setupNewBookingFields;
+import static org.assertj.core.api.FactoryBasedNavigableListAssert.assertThat;
 
-public class BaseBookingTest {
-    protected APIClient apiClient;
+public class BaseBookingTest extends BaseTest{
+    protected int createdBookingId;
+    protected ObjectMapper objectMapper = new ObjectMapper();
 
     @BeforeEach
-    public void setup() {
-        apiClient = new APIClient();
-        apiClient.createToken("admin", "password123");
+    public void createNewBookingPrecondition() throws JsonProcessingException {
+        NewBooking newBooking = setupNewBookingFields();
+        String requestBody = objectMapper.writeValueAsString(newBooking);
+        Response createdBookingResponse = apiClient.createBooking(requestBody);
+       Assertions.assertThat(createdBookingResponse.getStatusCode()).isEqualTo(200);
+
+        String responseBody = createdBookingResponse.asString(); //Приводим объект response к строке, для последующей десериализации
+        CreatedBooking createdBooking = objectMapper.readValue(responseBody, CreatedBooking.class); //Десериализуем респонс
+        createdBookingId = createdBooking.getBookingid();
+
     }
+
+    @AfterEach
+    public void tearDown() {
+        apiClient.createToken("admin", "password123");
+        apiClient.deleteBookingById(createdBookingId); //Вызываем метод delete для созданного в рамках теста bookingId
+
+        AssertionsForClassTypes.assertThat(apiClient.getBookingById(createdBookingId).getStatusCode()).isEqualTo(404); //Проверяем, что bookingId действительно удален
+    }
+
+
 }
